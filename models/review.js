b446284f8fe8de1d8535bbcfc37c70e33af369f0
jsb <tpqls1703@gmail.com>
2025-12-14 16:52:21 +0900

    feat: User-Book-Review 연관관계 정의

--- models/review.js
+++ models/review.js
@@ -10,7 +10,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+        Review.belongsTo(models.User, { foreignKey: 'user_id' });
+        Review.belongsTo(models.Book, { foreignKey: 'book_id' });
+
     }
   }
   Review.init({
